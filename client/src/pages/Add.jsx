f9520a85251cd96ec3db1a740f59cec1a4ad0ab2
eduvinagre <eduvinagre@gmail.com>
2023-05-14 14:44:42 -0300

    csv import

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import Papa from "papaparse";
 
 const Add = () => {
-  return <div>Add</div>;
+  // State to store parsed data
+  const [parsedData, setParsedData] = useState([]);
+
+  //State to store table Column name
+  const [tableRows, setTableRows] = useState([]);
+
+  //State to store the values
+  const [values, setValues] = useState([]);
+
+  const leArquivo = (e) => {
+    Papa.parse(e.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        const rowsArray = [];
+        const valuesArray = [];
+
+        // Iterating data to get column name and their values
+        results.data.map((d) => {
+          rowsArray.push(Object.keys(d));
+          valuesArray.push(Object.values(d));
+        });
+
+        // Parsed Data Response in array format
+        setParsedData(results.data);
+
+        // Filtered Column Names
+        setTableRows(rowsArray[0]);
+
+        // Filtered Values
+        setValues(valuesArray);
+      },
+    });
+  };
+
+  return (
+    <div className="form">
+      <h1>Adicionar Novo Produto</h1>
+      <input type="file" name="file" accept=".csv" onChange={leArquivo} />
+      <br />
+      <br />
+      {/* Table */}
+      <table>
+        <thead>
+          <tr>
+            {tableRows.map((rows, index) => {
+              return <th key={index}>{rows}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {values.map((value, index) => {
+            return (
+              <tr key={index}>
+                {value.map((val, i) => {
+                  return <td key={i}>{val}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Add;
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -16,7 +17,24 @@ const Products = () => {
     fetchAllProducts();
   }, []);
 
-  return <div>Products</div>;
+  return (
+    <div>
+      <h1>Produtos Shopper</h1>
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            {product.img && <img src={product.img} alt="" />}
+            <h2>{product.product_name}</h2>
+            <p>{product.product_code}</p>
+            <span>{product.product_price}</span>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Adicionar Novo Produto</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Products;
